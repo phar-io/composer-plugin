1e16f8ca01ad9151b29fcb44b9f7b24b4473150d
Tommy Muehle <tommy.muehle@gmail.com>
2017-02-16 15:41:45 +0100

    Update namespace and formatting for package classes

---
+++ src/Installer/PhivePackage.php
@@ -0,0 +1,23 @@
+<?php
+namespace PharIo\Composer\Installer;
+
+use Composer\Package\Package;
+
+class PhivePackage extends Package {
+
+    public function __construct() {
+        parent::__construct('phive', 'latest', 'latest');
+    }
+
+    public function getDistUrl() {
+        return 'https://phar.io/releases/phive.phar';
+    }
+
+    public function getDistUrls() {
+        return [$this->getDistUrl()];
+    }
+
+    public function getInstallationSource() {
+        return 'dist';
+    }
+}
